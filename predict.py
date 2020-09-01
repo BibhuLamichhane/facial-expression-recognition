50d558154acbbd4d2687304c42a3126eb28223ea
Bibhu Lamichhane <lamichhanebibhu0@gmail.com>
2020-09-01 17:02:34 +0545

    minor edits

---
+++ predict.py
@@ -0,0 +1,23 @@
+from tensorflow.keras.models import load_model
+
+# load the image
+# img = cv2.imread('images/test/disgust/2275.jpg', 0)
+
+
+def predict(img):
+    emotions = ['angry', 'disgust', 'fear', 'happy', 'neutral', 'sad', 'surprise']
+
+    # load model
+    mode = load_model('model.h5')
+
+    # image processing
+    img = img.reshape(1, 48, 48, 1)
+    img = img.astype('float64')
+
+    # prediction
+    predictions = mode.predict(img)
+    output = []
+    for prediction in range(len(predictions[0])):
+        text = f'{emotions[prediction].upper()}: {(predictions[0][prediction] * 100).round()}'
+        output.append(text)
+    return output
